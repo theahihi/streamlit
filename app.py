3e444c2e936028dab28a6b5b2bd98c1e60d65e9c
theahihi <168960744+theahihi@users.noreply.github.com>
2024-05-27 16:33:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ clf = pickle.load(open('iris_model.pkl', 'rb'))
 
 # Sidebar for user input
 st.sidebar.title('Iris Classifier')
-sepal_length = st.sidebar.slider('Sepal Length', 4.0, 8.0, 5.0)
-sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.5, 3.0)
-petal_length = st.sidebar.slider('Petal Length', 1.0, 7.0, 4.0)
-petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 1.0)
+sepal_length = st.sidebar.number_input('Sepal Length', min_value=4.0, max_value=8.0, value=5.0)
+sepal_width = st.sidebar.number_input('Sepal Width', min_value=2.0, max_value=4.5, value=3.0)
+petal_length = st.sidebar.number_input('Petal Length', min_value=1.0, max_value=7.0, value=4.0)
+petal_width = st.sidebar.number_input('Petal Width', min_value=0.1, max_value=2.5, value=1.0)
 
 # Make predictions
 prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
